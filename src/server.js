3b59f703b52c950c0e37eab728cc6f1b7c9084c8
tpwiedy <wiedy.pratama@gmail.com>
2024-08-31 22:05:07 +0700

    added registration function

--- src/server.js
+++ src/server.js
@@ -2,16 +2,24 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const ClientError = require('./exceptions/ClientError');
+
+// Albums
 const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const AlbumsValidator = require('./validator/albums');
+// Songs
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
+// Users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -38,6 +46,13 @@ const init = async () => {
         validator: SongsValidator,
       },
     },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      }
+    }
   ]);
 
   server.ext('onPreResponse', (request, h) => {
@@ -49,6 +64,7 @@ const init = async () => {
         message: response.message,
       });
       newResponse.code(response.statusCode);
+      console.log(response);
       return newResponse;
     }
     return h.continue;
